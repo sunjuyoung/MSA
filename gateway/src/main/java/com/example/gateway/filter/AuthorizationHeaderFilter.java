e36d2400d7600801ec2be92b4654e7823adba8f2
syseoz <syseoz@naver.com>
2022-04-30 23:30:22 +0900

    add gateway author filter

---
+++ gateway/src/main/java/com/example/gateway/filter/AuthorizationHeaderFilter.java
@@ -0,0 +1,73 @@
+package com.example.gateway.filter;
+
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHeaders;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+    private final Environment env;
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+            log.info("api author filter request id : {}",request.getId());
+            if(!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)){
+                return onError(exchange,"no authorization Header", HttpStatus.UNAUTHORIZED);
+            }
+            String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            String jwt = authorizationHeader.replace("Bearer","");
+            if(!isJwtValid(jwt)){
+                return onError(exchange,"jwt token is not valid", HttpStatus.UNAUTHORIZED);
+            }
+
+            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+
+            }));
+
+        });
+    }
+
+    private boolean isJwtValid(String jwt) {
+        boolean returnValue = true;
+        String subject = null;
+        try{
+            subject = Jwts.parser().setSigningKey(env.getProperty("token.secret"))
+                    .parseClaimsJws(jwt).getBody()
+                    .getSubject();
+        }catch (Exception e){
+            returnValue = false;
+        }
+        if(subject == null || subject.isEmpty()){
+            returnValue = false;
+        }
+        return returnValue;
+    }
+
+    //spring mvc 가 아닌 webflux 방식으로 비동기적으로 처리한다 Mono라는 단일값 반환
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        log.error(err);
+        return response.setComplete();
+    }
+
+    public static class Config{
+
+    }
+}
