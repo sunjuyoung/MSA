a0a12ac7ddec644219db42a9d7fafe82d59699f0
syseoz <syseoz@naver.com>
2022-04-30 17:11:10 +0900

    add user-service routes

---
+++ gateway/src/main/java/com/example/gateway/filter/GlobalFilter.java
@@ -0,0 +1,49 @@
+package com.example.gateway.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+
+    public GlobalFilter() {
+        super(Config.class);
+    }
+
+
+
+    @Override
+    public GatewayFilter apply(Config config) {
+
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+            log.info("global request id : {}",request.getId());
+
+
+            if(config.isPreLogger()){
+                log.info("global filter start : {}",request.getId());
+            }
+
+            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+                if(config.isPostLogger()){
+                    log.info("global filter end : {}",response.getStatusCode());
+                }
+            }));
+
+        });
+    }
+    @Data
+    public static class Config{
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+}
